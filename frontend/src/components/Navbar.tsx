824e97f21b4c5c27a35ce4f64a32030d109b5d6b
Daksh Vasala <vasaladaksh5@gmail.com>
2026-09-21 18:59:51 +0530

    refactor: clean up Navbar component structure for better readability

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,26 +13,28 @@ const Navbar = () => {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light border-bottom shadow-sm">
-      <div className="container">
-        <h3>Tasks</h3>
-        <button
-          className="btn btn-outline-danger ms-auto px-4"
-          type="button"
-          onClick={() => setModal(true)}
-        >
-          Logout
-        </button>
-      </div>
-    </nav>
-    <Modal isOpen={modal}>
-      <ModalHeader>Logout confirmation</ModalHeader>
-      <ModalBody>Are you sure you want to logout ?</ModalBody>
-      <ModalFooter>
-        <Button className="btn-danger" onClick={handleLogout}>Yes</Button>
-        <Button onClick={() => setModal(false)}>No</Button>
-      </ModalFooter>
-    </Modal>
+      <nav className="navbar navbar-expand-lg bg-light border-bottom shadow-sm">
+        <div className="container">
+          <h3>Tasks</h3>
+          <button
+            className="btn btn-outline-danger ms-auto px-4"
+            type="button"
+            onClick={() => setModal(true)}
+          >
+            Logout
+          </button>
+        </div>
+      </nav>
+      <Modal isOpen={modal}>
+        <ModalHeader>Logout confirmation</ModalHeader>
+        <ModalBody>Are you sure you want to logout ?</ModalBody>
+        <ModalFooter>
+          <Button className="btn-danger" onClick={handleLogout}>
+            Yes
+          </Button>
+          <Button onClick={() => setModal(false)}>No</Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
